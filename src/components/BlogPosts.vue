9aed48f47db506d2a308c128e31e48f92d340d01
C0nstal <constantin.s.01ta@gmail.com>
2024-12-17 06:56:20 +0300

    Initial commit

---
+++ src/components/BlogPosts.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Блог</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.body }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogPosts',
+  data() {
+    return {
+      posts: []
+    };
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        this.posts = data;
+      });
+  }
+};
+</script>
